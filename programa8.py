0b7aba4374464d0dd18f1cd944fd576fa74cac3c
GabrielaZarzozaPerez <55330340+GabrielaZarzozaPerez@users.noreply.github.com>
2019-09-16 16:43:54 -0500

    Create programa8.py
    Gabriela Zarzoza Perez 1853972 Gpo.22

---
+++ programa8.py
@@ -0,0 +1,11 @@
+  tiempo = (distancia / velocidad) 
+    return tiempo
+
+print("\nCuanta distancia vas a recorrer?")
+distancia = int(input())
+def formula():
+print("\nA que velocidad lo haras?")
+velocidad = int(input())
+
+
+print(f"\nEl tiempo para recorrer {distancia} kilometros a una velocidad de {velocidad}m/s es igual a:\n{formula()} segundos")
