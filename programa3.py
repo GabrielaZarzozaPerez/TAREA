8d4c2b07322b6af8106e82fde636200350c45c63
GabrielaZarzozaPerez <55330340+GabrielaZarzozaPerez@users.noreply.github.com>
2019-09-16 16:37:27 -0500

    Create programa3.py
    Gabriela Zarzoza Perez
    1853972
    Gpo.22

---
+++ programa3.py
@@ -0,0 +1,10 @@
+print("Pon un numero")
+Numero1 = input()
+
+print("\nPon otro numero")
+Numero2 = input()
+
+if Numero1 < Numero2:
+    print(f"\n{Numero1} es menor que {Numero2}")
+else:
+    print(f"\n{Numero1} es mayor que {Numero2}")
